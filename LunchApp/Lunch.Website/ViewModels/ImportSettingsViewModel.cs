38fbf0cf77b7b8ec0ee66d7ccd2a63516418d774
Marko Mijuskovic <marko.mijuskovic@gmail.com>
2012-12-21 16:28:18 -0500

    Working on logic to import restaurants from some API

---
+++ LunchApp/Lunch.Website/ViewModels/ImportSettingsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lunch.Website.ViewModels
+{
+    public class ImportSettingsViewModel
+    {
+        [Required]
+        [Display(Name="Use Defaults?:")]
+        public bool UseDefaults { get; set; }
+        [Required]
+        [Display(Name = "Publisher Key:")]
+        public string PublisherKey { get; set; }
+        [Required]
+        [Display(Name = "Latitude:")]
+        public string Latitude { get; set; }
+        [Required]
+        [Display(Name = "Longitude:")]
+        public string Longitude { get; set; }
+        [Required]
+        [Display(Name = "Radius:")]
+        public string Radius { get; set; }
+    }
+}
