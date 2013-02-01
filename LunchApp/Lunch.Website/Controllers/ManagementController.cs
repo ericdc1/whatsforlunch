d0c80076b15070fe32402b405495eb790b613336
Shawn King <shawnking@outlook.com>
2013-02-01 14:44:29 -0500

    Whats For Lunch - Mass CSS Structure Shift
    Removed many Media Queries and finished homepage layout

--- LunchApp/Lunch.Website/Controllers/ManagementController.cs
+++ LunchApp/Lunch.Website/Controllers/ManagementController.cs
@@ -33,6 +33,11 @@ namespace Lunch.Website.Controllers
 
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult ImportSettings()
+        {
             var defaults = GetDefaultImportSetting();
             var model = new RestaurantImportSettings
             {
