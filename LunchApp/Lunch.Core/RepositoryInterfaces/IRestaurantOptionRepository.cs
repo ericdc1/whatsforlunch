84e4b5c480d6468cb19542137660c350924748d0
jdehlin <jdehlin@gmail.com>
2013-02-01 14:11:21 -0500

    Methods for getting and saving restaurant options for a day Methods for getting top restaurants by votes for a day

---
+++ LunchApp/Lunch.Core/RepositoryInterfaces/IRestaurantOptionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Lunch.Core.Models;
+
+namespace Lunch.Core.RepositoryInterfaces
+{
+    public interface IRestaurantOptionRepository
+    {
+        IEnumerable<RestaurantOption> GetAllByDate(DateTime dateTime);
+        IEnumerable<RestaurantOption> GetAll();
+        RestaurantOption SaveOrUpdate(RestaurantOption entity);
+    }
+}
