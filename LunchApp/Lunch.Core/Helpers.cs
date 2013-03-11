b1f056e031de2ab70b7d81bafed4da29c4c7d6b1
Eric Coffman <ecoffman@hsc.wvu.edu>
2013-03-11 11:57:59 -0400

    fix DST bug

--- LunchApp/Lunch.Core/Helpers.cs
+++ LunchApp/Lunch.Core/Helpers.cs
@@ -9,26 +9,62 @@ namespace Lunch.Core
     public static class Helpers
     {
 
-        public static DateTime AdjustTimeOffsetFromUtc(DateTime time)
-        {
-            var utc = DateTime.UtcNow;
-            var eastern = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-            var easterntime = utc.Add(eastern.BaseUtcOffset);
+        //public static DateTime AdjustTimeOffsetFromUtc(DateTime time)
+        //{
+        //    var utc = DateTime.UtcNow;
+        //    var eastern = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+        //    var easterntime = utc.Add(eastern.BaseUtcOffset);
 
-            TimeSpan diff = (easterntime - utc);
+        //    TimeSpan diff = (easterntime - utc);
+        //    double hours = diff.TotalHours;
+        //    return time.AddHours(hours);
+        //}
+
+        public static DateTime AdjustTimeOffsetFromUtc(DateTime time, string zoneName = "Eastern Standard Time")
+        {
+
+            var x = TimeZoneInfo.GetSystemTimeZones();
+            var utc = DateTime.UtcNow;
+            //get the current timezone info
+            var zoneinfo = TimeZoneInfo.FindSystemTimeZoneById(zoneName);
+            //get the daylight savings time zone name
+            var daylightname = zoneinfo.DaylightName;
+            //get the time info
+            var timeinfo = utc.Add(zoneinfo.BaseUtcOffset);
+            //find out if the current time is in DST range
+            var isDaylight = zoneinfo.IsDaylightSavingTime(timeinfo);
+
+            //if the time is in DST range change zones to the DST zone
+            if (isDaylight)
+            {
+                timeinfo = utc.Add(zoneinfo.BaseUtcOffset).AddHours(1);
+            }
+
+            TimeSpan diff = (timeinfo - utc);
             double hours = diff.TotalHours;
             return time.AddHours(hours);
         }
 
-        public static DateTime AdjustTimeOffsetToUtc(DateTime time)
+
+        public static DateTime AdjustTimeOffsetToUtc(DateTime time, string zoneName = "Eastern Standard Time")
         {
             var utc = DateTime.UtcNow;
-            var eastern = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            //get the current timezone info
+            var zoneinfo = TimeZoneInfo.FindSystemTimeZoneById(zoneName);
+            //get the daylight savings time zone name
+            var daylightname = zoneinfo.DaylightName;
+            //get the time info
+            var timeinfo = utc.Add(zoneinfo.BaseUtcOffset);
+            //find out if the current time is in DST range
+            var isDaylight = zoneinfo.IsDaylightSavingTime(timeinfo);
+           
+            //if the time is in DST range change zones to the DST zone
+            if(isDaylight)
+            {
+                timeinfo = utc.Add(zoneinfo.BaseUtcOffset).AddHours(1);
+            }
 
-            var easterntime = utc.Add(eastern.BaseUtcOffset);
-            //var isDaylight = eastern.IsDaylightSavingTime(easterntime);
-
-            TimeSpan diff = (utc - easterntime);
+            TimeSpan diff = (utc - timeinfo);
             double hours = diff.TotalHours;
             return time.AddHours(hours);
         }
