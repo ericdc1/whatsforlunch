aaff3647ed9d165959734da597c7d2514061813d
Eric Coffman <ecoffman@hsc.wvu.edu>
2013-03-15 13:26:10 -0400

    added bootstrap and start of installer

---
+++ LunchApp/Lunch.Website/Controllers/SetupWizardController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lunch.Website.Controllers
+{
+    public class SetupWizardController : Controller
+    {
+        private readonly bool _setupComplete = Convert.ToBoolean(ConfigurationManager.AppSettings["SetupComplete"]);
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["AzureSQL"].ConnectionString;
+
+        public ActionResult Index()
+        {
+            if (_setupComplete)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if (_connectionString.Length > 5)
+            {
+                
+            }
+            return View();
+        }
+
+    }
+}
